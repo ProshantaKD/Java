7fcf86e1355f220b67d0e58752d5e7a1251b30da
ProshantaKD <proshantakd2004@gmail.com>
2025-12-13 22:58:51 +0600

    Java add file

---
+++ Concatenation.java
@@ -0,0 +1,9 @@
+public class Concatenation {
+    public static void main(String args[]) {
+        String firstName = "Proshanta";
+        String lastName = "Das";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+
+}
