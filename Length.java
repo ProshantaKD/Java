68b853affa2851b48999d0f61f0434833fd113f6
ProshantaKD <proshantakd2004@gmail.com>
2025-12-13 23:19:03 +0600

    Java add file

---
+++ Length.java
@@ -0,0 +1,10 @@
+public class Length {
+    public static void main(String args[]) {
+        String firstName = "Proshanta";
+        String lastName = "Das";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+        System.out.println(fullName.length());
+    }
+
+}
