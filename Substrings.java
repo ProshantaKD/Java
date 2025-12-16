5f19e7d25f88535cef7415018b54e443be056e7e
ProshantaKD <proshantakd2004@gmail.com>
2025-12-16 13:25:59 +0600

    New code for practice

---
+++ Substrings.java
@@ -0,0 +1,9 @@
+public class Substrings {
+    public static void main(String args[]) {
+        String sentence = "My name is Proshanta";
+        String name = sentence.substring(11, sentence.length());
+        ;
+        System.out.println(name);
+    }
+
+}
