6f2a73ba4ac171d6cc4cbb9e99f675c27c606780
ProshantaKD <proshantakd2004@gmail.com>
2025-12-16 13:35:15 +0600

    New code

---
+++ Charat.java
@@ -0,0 +1,10 @@
+public class Charat {
+    public static void main(String args[]) {
+        String name = "Proshanta Kumar Das";
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }
+
+    }
+
+}
