248bfdcb290895452bb59947c2ae36c2d677ee17
ProshantaKD <proshantakd2004@gmail.com>
2025-12-16 13:17:14 +0600

    New file added

---
+++ Compare.java
@@ -0,0 +1,12 @@
+public class Compare {
+    public static void main(String args[]) {
+        String name1 = "Proshanta";
+        String name2 = "Suparna";
+        if (name1.compareTo(name2) == 0) {
+            System.out.println("String are Equal");
+        } else {
+            System.out.println("String are not Equal");
+        }
+    }
+
+}
